4ec029cd34745a39adfe73b5c1f0723a06f8fc8b
Fiobs <zav74vaz@gmail.com>
2023-04-01 22:58:54 +0300

    change base dir

--- server_checker/urls.py
+++ server_checker/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from server_status.views import ServerViewSet
+
+router = routers.DefaultRouter()
+router.register(r'servers', ServerViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
